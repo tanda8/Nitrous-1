daae530a0f6e16ba085405cbf00ce41fbfcf2ac6
tanda8 <tanda88@gmail.com>
2014-01-17 15:11:17 +0000

    Rails Tutorial 2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,22 @@ class PostsController < ApplicationController
   end
   
   def create
-    render text: params[:post].inspect
+   # render text: params[:post].inspect
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
   end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def index
+    @posts = Post.all
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end  
 end
